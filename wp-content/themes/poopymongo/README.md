e84210de787e0e4582879345a58e9a9da84c9dbc
heayeon-smf <heayeon@sanbornmediafactory.com>
2016-08-18 04:16:29 -0400

    added starter files

---
+++ wp-content/themes/poopymongo/README.md
@@ -0,0 +1,2 @@
+# Wordpress Starter Theme
+This sass-based WP template is meant to serve as a very stripped-down, light-weight starter. 
---
+++ wp-content/themes/poopymongo/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// Requirements
+require(get_template_directory().'/inc/vars.php');
+//require(get_template_directory().'/inc/taxonomies.php');
+//require(get_template_directory().'/inc/post_types.php');
+require(get_template_directory().'/inc/acf.php');
+require(get_template_directory().'/inc/misc_functions.php');
+require(get_template_directory().'/inc/images.php');
+require(get_template_directory().'/inc/menus.php');
+require(get_template_directory().'/inc/widgets.php');
+
+// Admin Requirements
+if(is_admin()){
+    
+}
+
+?>
